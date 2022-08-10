a5c7807d765c8c27656472df1010f415e7f46928
Henry <hspradley91@gmail.com>
2022-08-09 20:47:23 -0500

    Completed exercise 16-6.

---
+++ 16-6_refactoring.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = 'data/eq_data_30_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+    
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+
+for eq_dict in all_eq_dicts:
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
+    
+data = [{
+    'type':'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker':{
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'YlOrRd', #'Blackbody',
+        'reversescale': False,
+        'colorbar': {'title': 'Magnitude'},
+    },
+}]
+my_layout = Layout(title='Global Earthquakes', title_x=0.5)
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='16-6_global_earthquakes.html')
